124897a628ba0f838b77f58ef47deec2e32905c0
svetlakov.a <svetlakov.andrey0301@gmail.com>
2024-12-24 19:22:33 +0300

    1. Добавлена запись имен этажей в 2 разных параметра. 2. Отлажена запись имен этажей. 3. Добавлены оповещения Notification.WPF.

--- asBIM/Cls_SetMaxMinPtToElements/Code_SetMaxMinPtToElements.cs
+++ asBIM/Cls_SetMaxMinPtToElements/Code_SetMaxMinPtToElements.cs
@@ -13,6 +13,7 @@ using System.Xaml;
 using System.Windows.Documents;
 using System.Xml.Linq;
 using System.Windows;
+using Notifications.Wpf;
 
 
 namespace asBIM
@@ -36,9 +37,16 @@ namespace asBIM
             SetElementsTBPoints(doc);
             
             // // TODO: 12. В релизе убрать вывод инфо об Уровнях
-            LevelInfo.GetLevelInfoInDoc(doc);
+            //LevelInfo.GetLevelInfoInDoc(doc);
 
             // TODO: 8. Сделать вывод инфо: 1. Необработанные элементы, 2. Обработанные элементы, 3. Время работы команды
+            // Оповещение об успешной отработке команды
+            NotificationManagerWPF_SetMaxMinPtToElements.Success(success: message);
+            // Оповещение об ошибке
+            //NotificationManagerWPF_SetMaxMinPtToElements.Error(error: message);
+            // Оповещение о времени работы команды
+            
+            
 
             // ОСНОВНОЙ КОД ПЛАГИНА // КОНЕЦ  
             return Result.Succeeded;
@@ -51,9 +59,9 @@ namespace asBIM
         {
             // Коллекция + словать с именем переменной groupedElements с фильтром на категорию и Элементы
             var groupedElements = RevitAPI_Sort_ByCategory.SortElementByCategory(doc);
-            // TODO: 10. Вынести транзакцию из режима "для каждого элемента"
+
             //Транзакция
-            using (Transaction tr = new Transaction(doc, "Запись параметра"))
+            using (Transaction tr = new Transaction(doc, "Запись параметров отметок Верха и Низа"))
             {
                 tr.Start();
                 // Цикл с перебором всех элементов в коллекции
@@ -68,10 +76,13 @@ namespace asBIM
                         // TODO: 11. Добавить запись уровня для отметки Верха. Протестировать.
                         
                         //Получение "Общего параметра по Guid" из Revit в переменную levelInfo параметра из эл в коллекции. Уровень для отметки Низа
-                        Parameter levelInfo = elemincollector.get_Parameter(new Guid("bade3ed8-b24b-426e-a430-99deb348bb38")); // Общий параметр - PRO_Уровень низа
+                        Parameter topPtLevelInfo = elemincollector.get_Parameter(new Guid("ebb82c57-a7f5-4152-8a3c-6572cc32f6f1")); // Общий параметр - PRO_Уровень верха
+                        
+                        //Получение "Общего параметра по Guid" из Revit в переменную levelInfo параметра из эл в коллекции. Уровень для отметки Низа
+                        Parameter botPtLevelInfo = elemincollector.get_Parameter(new Guid("bade3ed8-b24b-426e-a430-99deb348bb38")); // Общий параметр - PRO_Уровень низа
                         
                         // Проверка на null "Общего параметра по Guid" из Revit
-                        if (topPointParam != null && bottomPointParam != null && levelInfo != null)
+                        if (topPointParam != null && bottomPointParam != null && botPtLevelInfo != null)
                         {
                             // Конвертация единиц из Футов в ММ. Для точки Верха
                             var top = UnitUtils.ConvertFromInternalUnits(ElementTopBottomPt.GetElementTopPoint(elemincollector).Z, UnitTypeId.Millimeters);
@@ -82,27 +93,40 @@ namespace asBIM
                             // Округление botVal до 2 знаков после запятой
                             var botVal = Math.Round(bot, 2, MidpointRounding.AwayFromZero);
 
-                            // Запись значения из GetElementTopPoint(elemincollector).Z в параметр topPointParam - отметки Верха с конвертацией в строку
+                            // Запись значения из GetElementTopPoint(elemincollector).Z
+                            // в параметр topPointParam - отметки Верха с конвертацией в строку
                             topPointParam.Set(Convert.ToString(topVal));
-                            // Запись значения из GetElementBottomPoint(elemincollector).Z в параметр bottomPointParam - отметки Низа с конвертацией в строку
+                            // Запись значения из GetElementBottomPoint(elemincollector).Z
+                            // в параметр bottomPointParam - отметки Низа с конвертацией в строку
                             bottomPointParam.Set(Convert.ToString(botVal));
                             
                             // Фильтр на Уровни. Выборка из Документа только уровней.
                             FilteredElementCollector levelsCollector = new FilteredElementCollector(doc).OfClass(typeof(Level));
                             // Список levels с типом Level
                             List<Level> levels = levelsCollector.Cast<Level>().OrderBy(level => level.Elevation).ToList();
-
+                            
+                            
+                            // ЗАПИСЬ УРОВНЯ ДЛЯ ОТМЕТКИ ВЕРХА
                             // Получение отметки Верха элемента с каждого элемента в Документе.
+                            double elementTopPointElevationSm = ElementTopBottomPt.GetElementTopPoint(elemincollector).Z;
+                            // Присвоение closestLevel результата метода FindBottomElemLevel
+                            // В метод FindBottomElemLevel подаются отметки Низа всех эл из groupedElements["AR"] и отсортированный по возрастанию??? список уровней
+                            Level closestLevelForTop = LevelInfo.FindBottomElemLevel(elementTopPointElevationSm, levels);
+                            // Запись Имени Нижнего этажа.
+                            topPtLevelInfo.Set(closestLevelForTop != null ? closestLevelForTop.Name : "Не определено");
+                            // ЗАПИСЬ УРОВНЯ ДЛЯ ОТМЕТКИ ВЕРХА
+                            
+                            
+                            // ЗАПИСЬ УРОВНЯ ДЛЯ ОТМЕТКИ НИЗА
+                            // Получение отметки Низа элемента с каждого элемента в Документе.
                             double elementBottomPointElevationSm = ElementTopBottomPt.GetElementBottomPoint(elemincollector).Z;
                             // Присвоение closestLevel результата метода FindBottomElemLevel
                             // В метод FindBottomElemLevel подаются отметки Низа всех эл из groupedElements["AR"] и отсортированный по возрастанию??? список уровней
-                            Level closestLevel = LevelInfo.FindBottomElemLevel(elementBottomPointElevationSm, levels);
-                            // Запись Имени этажа. Тест.
+                            Level closestLevelForBot = LevelInfo.FindBottomElemLevel(elementBottomPointElevationSm, levels);
+                            // Запись Имени Нижнего этажа.
+                            botPtLevelInfo.Set(closestLevelForBot != null ? closestLevelForBot.Name : "Не определено");
+                            // ЗАПИСЬ УРОВНЯ ДЛЯ ОТМЕТКИ НИЗА
                             
-                            //var value = "Не определено";
-                            //if (closestLevel != null)
-                            //    value = closestLevel.Name;
-                            levelInfo.Set(closestLevel != null ? closestLevel.Name : "Не определено");
                             
                             // TODO: 13. Добавить обнулятор значений на время тестов
                         }
---
+++ asBIM/Helpers/Notifications.WPF.cs
@@ -0,0 +1,38 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
+using Notifications.Wpf.Controls;
+
+namespace Notifications.Wpf
+{
+    public static class NotificationManagerWPF_SetMaxMinPtToElements
+    {
+        public static void Success(this string success)
+        {
+            var notificationManager = new NotificationManager();
+            var ts = new TimeSpan(0, 0, 5);
+
+            notificationManager.Show(new NotificationContent
+            {
+                Title = "Отметки Верха и Низа элементов \nзаписаны!", 
+                Message = success, 
+                Type = NotificationType.Success
+            }, expirationTime: ts);
+        }
+        
+        public static void Error(this string error)
+        {
+            var notificationManager = new NotificationManager();
+            var ts = new TimeSpan(0, 0, 5);
+
+            notificationManager.Show(new NotificationContent
+            {
+                Title = "Ошибка!", 
+                Message = error, 
+                Type = NotificationType.Error
+            }, expirationTime: ts);
+        }
+    }
+}
