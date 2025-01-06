d4d9db9aa0a56b0c5dcb9abca6dd30a5aef17b9e
12kiw <svetlakov.andrey0301svetlakoff@yandex.ru>
2025-01-07 01:51:36 +0300

    Загрузка иконок

--- asBIM/Properties/Resources.Designer.cs
+++ asBIM/Properties/Resources.Designer.cs
@@ -63,6 +63,26 @@ namespace asBIM.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap PlaceGroupsInSpacesTX_16 {
+            get {
+                object obj = ResourceManager.GetObject("PlaceGroupsInSpacesTX_16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap PlaceGroupsInSpacesTX_32 {
+            get {
+                object obj = ResourceManager.GetObject("PlaceGroupsInSpacesTX_32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap TB_Points_16 {
             get {
                 object obj = ResourceManager.GetObject("TB_Points_16", resourceCulture);
